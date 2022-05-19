b424e55583de91cf7d84041a95fb62c3876b18cb
Terell <Terelld@gmail.com>
2022-05-19 10:01:57 -0500

    commit tree bafc4d6e643faf7a3b0a576537af13b6db899f0f

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply = (num1 * num2);
+let num1 = 31;
+let num2 = 2;
+multiply = 31 * 2;
 
-// Write your code here
+
+
+let random = Math.floor(Math.random() * 10) + 1 ;
+
+
+let mod = (num3 % num4);
+let num3 = 12;
+let num4 = 8;
+mod = 12 % 8;
+
+let max = Math.max(2,5,7,11,20);
+max;
